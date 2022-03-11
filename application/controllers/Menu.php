6d70ed6230c43cccf80f6e59a79d956e97b3e6ea
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2022-03-11 09:30:45 +0700

    Install Asset, UI + Integrasi CI

---
+++ application/controllers/Menu.php
@@ -0,0 +1,16 @@
+<?php 
+
+    
+    defined('BASEPATH') OR exit('No direct script access allowed');
+    
+    class Menu extends CI_Controller {
+    
+        public function index()
+        {
+            $this->load->view('lat_ui');
+        }
+    
+    }
+    
+    /* End of file Menu.php */
+    
