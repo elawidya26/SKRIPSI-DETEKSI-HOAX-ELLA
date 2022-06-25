1c53beed9967ffeccae7c47aa156a3ab48deedd3
elawidya26 <ellawidya91@gmil.com>
2022-06-25 19:47:29 +0700

    ico
    ico

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -49,7 +49,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<!--end::Global Theme Styles-->
 		<!--begin::Layout Themes(used by all pages)-->
 		<!--end::Layout Themes-->
-		<link rel="shortcut icon" href="<?php echo base_url() ?>assets/media/logos/favicon.ico" />
+		<link rel="shortcut icon" href="<?php echo base_url() ?>assets/media/logos/logopolinema.ico" />
 	</head>
 	<!--end::Head-->
 	<!--begin::Body-->
